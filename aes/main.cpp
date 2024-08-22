b997478d9e2ebf7428d5b037491acf73466d5d6c
MatthewQuilapio <156325023+MatthewQuilapio@users.noreply.github.com>
2024-08-23 00:44:49 +0800

    Add files via upload

--- aes/main.cpp
+++ aes/main.cpp
@@ -1,14 +1,21 @@
 #include "aes_header.h"
 #include "read_file_header.h"
 
-int size_input = 0;
+unsigned int size_input = 0;
 
-void readFile(std::string str_file_input)
+void readFile(std::string str_file_input, unsigned char *roundKeys)
 {
     std::filesystem::path path_file_input = str_file_input;
+    std::vector <std::string> splitted_inp = split_string(str_file_input, '\\');
+    std::string str_out_file = splitted_inp[splitted_inp.size() - 1];
+    std::vector <std::string> str_file = split_string(str_out_file, '.');
+    std::string str_out_filename = str_file[0];
+    std::string str_final_filename = str_out_filename + "_enc.txt";
     std::ifstream inFile;
-    std::cout << "Path: " << path_file_input << '\n'; 
-    inFile.open(str_file_input,std::ios_base::in);
+    std::ofstream outFile;
+    inFile.open(str_file_input, std::ios_base::in);
+    outFile.open(str_final_filename);
+    
 
     if (!inFile)
     {
@@ -18,58 +25,123 @@ void readFile(std::string str_file_input)
     else
     {
         std::string line;
-        int x = 0;
-        char *char_str_to_ptr;
         while (std::getline(inFile, line)) 
         {
            char *char_array_line = pad_16(line);
-           array_splitter(char_array_line);
+           std::vector<std::vector<unsigned char>> splitted_array = array_splitter(char_array_line);
+           unsigned char *out;
+           
+           for (const auto& chunk : splitted_array) 
+           {
+                unsigned char in[size_input + 1] = {};
+                unsigned char *out = new unsigned char[size_input];
+                int ctr = 0;
+                for(unsigned char c: chunk)
+                {
+                    in[ctr] = c;
+                    ctr++;
+                    if (ctr == size_input)
+                    {
+                        in[size_input] = '\0';
+                        break;
+                    }  
+                }
 
-           std::cout << char_array_line << '\n'; 
+                for (unsigned int i = 0; i < size_input; i += size_input) 
+                {
+                    encrypt_aes(in + i, out + i, roundKeys);
+                    //out_line.push_back(out[i]);
+                }
+                outFile << out;
+           }
+           outFile << '\n'; 
         }
-        
         
     }
 }
 
-void array_splitter(char *inp_array)
+std::vector<std::string> split_string(std::string str_inp, char splitter_val)
 {
-	unsigned int arr_len = sizeof(inp_array);
-
-    switch (size_input) 
-    {   
-		case BYTE_16:
+    int startIndex = 0;
+    int endIndex = 0;
+    std::vector<std::string> output_split;
+    for (int i = 0; i <= str_inp.size(); i++) 
+    {
+        
+        // If we reached the end of the word or the end of the input.
+        if (str_inp[i] == splitter_val || i == str_inp.size()) 
         {
-            unsigned int uint_group = arr_len/BYTE_16;
-            unsigned char array_storage[uint_group][BYTE_16];
-            for(int x = 0; x < uint_group; x++)
-            {
-                for(int y = 0; y < BYTE_16; y++)
-                {
-                    array_storage[x][y] = inp_array[y];
-                }
-            }
-
-            break;
+            endIndex = i;
+            std::string temp_Str;
+            temp_Str.append(str_inp, startIndex, endIndex - startIndex);
+            output_split.push_back(temp_Str);
+            startIndex = endIndex + 1;
         }
-		case BYTE_24:
-            unsigned int uint_group = arr_len/BYTE_24;
-            break;
-		case BYTE_32:
-            unsigned int uint_group = arr_len/BYTE_32;
-            break;
+    }
+    return output_split;
+}
 
-        default:
-            std::cerr << "You input the wrong size!!";
-	}
+std::vector<std::vector<unsigned char>> array_splitter(char *inp_array)
+{
+    unsigned int arr_len = strlen(inp_array);
+    std::vector<std::vector<unsigned char>> array_storage;
+
+
+    if (size_input == 16)
+    {
+        unsigned int uint_group = arr_len / BYTE_16;
+        array_storage.resize(uint_group, std::vector<unsigned char>(BYTE_16));
+        for (int x = 0; x < uint_group; x++)
+        {
+            for (int y = 0; y < BYTE_16; y++)
+            {
+                array_storage[x][y] = inp_array[(BYTE_16 * x) + y];
+            }
+        }
+   
+    }
+    else if (size_input == 24)
+    {
+        unsigned int uint_group = arr_len / BYTE_24;
+        array_storage.resize(uint_group, std::vector<unsigned char>(BYTE_24));
+        for (int x = 0; x < uint_group; x++)
+        {
+            for (int y = 0; y < BYTE_24; y++)
+            {
+                array_storage[x][y] = inp_array[(BYTE_24 * x) + y];
+            }
+        }
+
+    }
+    else if (size_input == 32)
+    {
+        unsigned int uint_group = arr_len / BYTE_32;
+        array_storage.resize(uint_group, std::vector<unsigned char>(BYTE_32));
+        for (int x = 0; x < uint_group; x++)
+        {
+            for (int y = 0; y < BYTE_32; y++)
+            {
+                array_storage[x][y] = inp_array[(BYTE_32 * x) + y];
+            }
+        }
+
+    }
+    else
+    {
+        // Handle invalid size_input if needed
+        std::cerr << "Invalid size_input value" << std::endl;
+    }
+
+    return array_storage;
+
 }
 
 int main()
 {
     std::string str_file_input;
-    int *int_key;
-    unsigned int *int_initial_byte;
-    unsigned int *int_expanded_key;
+    //unsigned char uint_key[];
+    unsigned char *uint_initial_byte;
+    unsigned char *int_expanded_key;
 
     printf("Please Enter File: \n");
     std::cin >> str_file_input;
@@ -78,17 +150,19 @@ int main()
     std::cin >> size_input;
 
     
-    int_key = get_random_bytes(BYTE_16);
-    int_initial_byte = aes_initialize(sizeof(int_key));
-    
-    key_expansion(reinterpret_cast <unsigned int*> (int_key), reinterpret_cast <unsigned int*> (int_initial_byte));
+    unsigned char uint_key[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+    //get_random_bytes(size_input);
+    aes_initialize(size_input);
+    unsigned char *roundKeys = new unsigned char[4 * Nb * (Nr + 1)];
+    key_expansion(uint_key, roundKeys);
 
-    std::cout << int_key << '\n';  
+    //std::cout << int_key << '\n';  
     /* for(int i = 0; i < BYTE_24; i++)
     {
         std::cout << std::hex << int_key[i] << '\n';   
     } */
-    readFile(str_file_input);
+    readFile(str_file_input, roundKeys);
+
     
     return 0;
     
--- aes/read_file_header.h
+++ aes/read_file_header.h
@@ -1,5 +1,7 @@
 #include "aes_header.h"
 
-void readFile(std::string str_file_input);
+void readFile(std::string str_file_input, unsigned char *roundKeys);
 void printFormat(std::string str_file_input);
-void array_splitter(char *inp_array);
+std::vector<std::vector<unsigned char>> array_splitter(char *inp_array);
+std::vector<std::string> split_string(std::string str_inp, char splitter_val);
+
